ffb1236bcd7342d03a47ca9a34f3cb33a3d5360e
seoyoung <ww0111@naver.com>
2019-01-25 15:20:42 +0900

    logging 처리

--- shop/views.py
+++ shop/views.py
@@ -1,6 +1,9 @@
+import logging
 from django.http import HttpResponse
 from django.shortcuts import render
 from .models import Item
+
+logger = logging.getLogger(__name__)   #__name__ ==> "shop.views"
 
 def archives_year(request, year):
     return HttpResponse('{}년도에 대한 내용'.format(year))
@@ -11,6 +14,8 @@ def item_list(request):
     q = request.GET.get('q', '')
     if q:
         qs = qs.filter(name__icontains=q)
+
+    logger.debug('query : {}'.format(q))
 
     return render(request, 'shop/item_list.html', {
         'item_list' : qs,
