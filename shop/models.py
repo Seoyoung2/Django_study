12e050471038b2ca79be04cb5ad645e757b4f920
seoyoung <ww0111@naver.com>
2019-01-27 19:43:38 +0900

    URL Reverse 활용 ( get_absolute_url)

--- shop/models.py
+++ shop/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 from askcompany_django.utils import uuid_upload_to
 
 
@@ -14,4 +15,8 @@ class Item(models.Model):
     def __str__(self):
         #return '<{}> {}'.format(self.pk, self.name)
         return f'<{self.pk}> {self.name}'
+
+    def get_absolute_url(self):
+        #return reverse('shop:item_detail', args=[self.pk])
+        return reverse('shop:item_detail', kwargs={'pk': self.pk})
         
