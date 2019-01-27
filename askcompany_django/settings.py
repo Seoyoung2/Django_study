744c3e844c86b7eab715c581400f8d6aa19c8ad4
seoyoung <ww0111@naver.com>
2019-01-27 16:29:48 +0900

    media파일 다루기

--- askcompany_django/settings.py
+++ askcompany_django/settings.py
@@ -150,6 +150,8 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'askcompany_django', 'static'),
 ]
 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 INTERNAL_IPS = ['127.0.0.1']
 
--- blog/models.py
+++ blog/models.py
@@ -1,9 +1,11 @@
 from django.db import models
+from askcompany_django.utils import uuid_upload_to
 
 class Post(models.Model):
     author_name = models.CharField(max_length=20)
     title = models.CharField(max_length=100)
     content = models.TextField()
+    photh = models.ImageField(upload_to=uuid_upload_to)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
